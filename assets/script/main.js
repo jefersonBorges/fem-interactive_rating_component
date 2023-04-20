654fbe35cd5b6105e2ea149ef6259e1569323095
Jeferson Borges <j3f3rs0n.b0rg3s@gmail.com>
2023-04-20 15:30:06 -0300

    finishing desktop visibility and js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,17 +1,51 @@
 
 const Rating = {
+  
   init: function(){
     this.cache_selectors()
-    console.log(this)
+    this.bind_events()
+    this.set_initial_card_state()
   },
+
   cache_selectors: function(){
-    this.$rating_card = document.querySelector('#rating')
-    this.$rating_form = document.querySelector('#rating_form')
-    this.$rating_value = document.getElementsByName('rating_value')
-    this.$submit_button = document.querySelector('#submit_button')
-    this.$thanks_card = document.querySelector('#thanks')
-    this.$rated = document.querySelector('#rated')
+    this.$card_rating = document.querySelector('#card_rating')
+    this.$form_rating = document.querySelector('#form_rating')
+    this.$card_thanks = document.querySelector('#card_thanks')
+    this.$submitted_rating = document.querySelector('#submitted_rating')
   },
+
+  bind_events: function(){
+    this.$form_rating.onsubmit = this.submit_rating.bind(this)
+  },
+
+  set_initial_card_state: function(){
+    this.element_display_toggle(this.$card_thanks)
+  },
+
+  submit_rating: function(e){
+    e.preventDefault()
+
+    const submittedRating = document.querySelector('input[name="rating_value"]:checked')
+
+    if(submittedRating){
+      
+      this.element_display_toggle(this.$card_rating)
+      this.element_display_toggle(this.$card_thanks)
+
+      this.display_submitted_rating(submittedRating.value)
+
+    } else { /*do nothing until one rating is selected*/ }
+  },
+
+  element_display_toggle: function(element){
+    element.style.display === 'none' ?
+      element.style.display = 'flex' :
+      element.style.display = 'none'
+  },
+
+  display_submitted_rating: function(submittedRating){
+    this.$submitted_rating.innerText = submittedRating
+  }
 }
 
 Rating.init()
